5bc06154a74d7b4fe4e1bbf1ac35a907125ecb7a
Abhishek Bhargav <39580132+abhibhargav29@users.noreply.github.com>
2021-04-30 09:26:30 +0530

    Update FlaskApp.py

--- Flask App/FlaskApp.py
+++ Flask App/FlaskApp.py
@@ -5,6 +5,27 @@ from sklearn.preprocessing import normalize
 import pandas
 import sys
 
+#Defibe flask app
+app = Flask(__name__)
+
+#This try except is for the case you open the whole project folder is VS code 
+flag=0 
+try:
+    with open("MainPickle.pkl","rb") as file1:
+        Model,bow_model,OldData = pickle.load(file1)
+    with open("StopStem.pkl","rb") as file2:
+        stop,sno =pickle.load(file2)
+except:
+    with open("Flask App/MainPickle.pkl","rb") as file1:
+        Model,bow_model,OldData = pickle.load(file1)
+    with open("Flask App/StopStem.pkl","rb") as file2:
+        stop,sno =pickle.load(file2)
+    flag=1
+        
+user_entry = None
+emotionDict = {0:"anger", 1:"fear", 2:"joy", 3:"sad", "anger":0, "fear":1, "joy":2, "sad":3}
+    
+#Main Window
 @app.route("/", methods=["GET","POST"])
 def mainWin():
     global Model,bow_model
@@ -16,16 +37,16 @@ def mainWin():
         text = request.form["text"]
         filtered_text = clean(text, sno, stop)
         X = normalize(bow_model.transform([filtered_text])).tocsr()
-        user_entry = X
         Y = Model.predict(X)[0]
         res = emotionDict[Y]
         user_entry = filtered_text
 
     return render_template(("main.html"), result=res)
 
+#Edit Window
 @app.route("/edit", methods=["GET","POST"])
 def editAns():
-    global Model,bow_model,OldData
+    global Model,bow_model,OldData,flag
     global sno, stop
     global user_entry, emotionDict
 
@@ -34,10 +55,7 @@ def editAns():
         NewData = pandas.DataFrame()
         NewData["text"] = [user_entry]
         NewData["emotion"] = [emotionDict[ans]] 
-        Data = pandas.concat([OldData, NewData], ignore_index=True)
-        OldData = Data
-        print(Data)
-        print(OldData)
+        OldData = pandas.concat([OldData, NewData], ignore_index=True)
         bow_model.fit(OldData["text"])
         X = normalize(bow_model.transform(OldData["text"])).tocsr()
         Y = OldData["emotion"].to_numpy()
@@ -53,6 +71,7 @@ def editAns():
 
     return render_template(("edit.html"))
 
+#Cleaning function
 def clean(text, sno, stop):
     tags = re.compile("^@[a-zA-Z_]*")
     text = re.sub(tags," ",text)
@@ -72,24 +91,6 @@ def clean(text, sno, stop):
     
     return filtered_text
 
-
+#driver code
 if __name__=="__main__":
-    app = Flask(__name__)
-    
-    flag=0 
-    try:
-        with open("MainPickle.pkl","rb") as file1:
-            Model,bow_model,OldData = pickle.load(file1)
-        with open("StopStem.pkl","rb") as file2:
-            stop,sno =pickle.load(file2)
-    except:
-        with open("Flask App/MainPickle.pkl","rb") as file1:
-            Model,bow_model,OldData = pickle.load(file1)
-        with open("Flask App/StopStem.pkl","rb") as file2:
-            stop,sno =pickle.load(file2)
-        flag=1
-        
-    user_entry = None
-    emotionDict = {0:"anger", 1:"fear", 2:"joy", 3:"sad", "anger":0, "fear":1, "joy":2, "sad":3}
-    
     app.run(debug=True)
